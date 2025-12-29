dfd482398c33eff4311a95559a6d797f31880127
Joe <167563398+Joercat@users.noreply.github.com>
2025-12-29 16:48:49 -0500

    Create timer.h

---
+++ drivers/timer.h
@@ -0,0 +1,39 @@
+// SCos Timer Driver
+// Programmable Interval Timer (PIT) support
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "../kernel/kernel.h"
+#include "../kernel/io.h"
+
+// PIT ports
+#define PIT_CHANNEL0    0x40
+#define PIT_CHANNEL1    0x41
+#define PIT_CHANNEL2    0x42
+#define PIT_COMMAND     0x43
+
+// PIT frequency
+#define PIT_FREQUENCY   1193182
+
+namespace Timer {
+    // Initialize timer with given frequency (Hz)
+    void init(uint32_t frequency);
+    
+    // Timer tick handling
+    void handle_interrupt();
+    
+    // Time functions
+    uint64_t get_ticks();
+    uint32_t get_seconds();
+    uint32_t get_milliseconds();
+    
+    // Delay functions
+    void sleep(uint32_t milliseconds);
+    void sleep_ticks(uint32_t ticks);
+    
+    // Timer frequency
+    uint32_t get_frequency();
+}
+
+#endif // TIMER_H
