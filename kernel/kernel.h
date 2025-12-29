4f3eb293e3d27c6c2a8274c4b26dd904678c9ab5
Joe <167563398+Joercat@users.noreply.github.com>
2025-12-29 17:24:41 -0500

    Update kernel.h

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -4,38 +4,23 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-#include "memory.h"
-#include "string.h"
+#include "types.h"
 #include "io.h"
+#include "string.h"
+#include "memory.h"
 
-// Standard type definitions
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+// ============================================================================
+// SCos color scheme (VGA text mode attributes)
+// ============================================================================
 
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef signed long long int64_t;
-
-typedef uint32_t size_t;
-typedef int32_t ssize_t;
-
-// Boolean type
-typedef uint8_t bool;
-#define true 1
-#define false 0
-
-// NULL pointer
-#define NULL ((void*)0)
-
-// SCos color scheme
 #define SCOS_NEON_GREEN    0x0A    // Light green on black
 #define SCOS_DARK_GREEN    0x02    // Dark green on black
-#define SCOS_BRIGHT_GREEN  0x0F    // Bright white (will use for highlights)
+#define SCOS_BRIGHT_GREEN  0x0F    // Bright white (for highlights)
 
+// ============================================================================
 // Kernel functions
+// ============================================================================
+
 extern "C" void kernel_main(uint32_t magic, uint32_t* multiboot_info);
 void kernel_init();
 void kernel_panic(const char* message);
